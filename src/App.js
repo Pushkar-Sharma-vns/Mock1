b18887aff1f614ee8c8598a807c8e73e76e4b9aa
Pushkar-Sharma-vns <pushkar.vns16@gmail.com>
2021-07-04 11:27:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavbarBeforeSignUp from './components/navbarBeforeSignUp';
+import NavbarAfterSignUp from './components/navbarAfterSignUp';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './screens/home';
+
+function App() {
+  return (
+    <div className="App">
+        <Home/>
+    </div>
+  );
+}
+export default App;
